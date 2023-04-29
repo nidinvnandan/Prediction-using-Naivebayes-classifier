80b34071a1f0d376a627ddb28cef6e646e841cac
nidinvnandan <128630742+nidinvnandan@users.noreply.github.com>
2023-04-29 16:26:28 +0530

    Update naive_bayes.py

--- naive_bayes.py
+++ naive_bayes.py
@@ -7,7 +7,7 @@ class naive_bayes:
     # Read our dataset using read_csv()
     bbc_text = pd.read_csv('bbc-text.txt')
     bbc_text=bbc_text.rename(columns = {'text': 'News_Headline'}, inplace = False)
-    bbc_text.head()
+    
     bbc_text.category = bbc_text.category.map({'tech':0, 'business':1, 'sport':2, 'entertainment':3, 'politics':4})
     bbc_text.category.unique()
 
